a4cf58a80b0d6eb1261fd3b91d738f00b87a3573
addaoud <ahmed0daoudad@gmail.com>
2025-06-15 02:23:29 -0600

    Update pipeline

--- data_scripts/clip.py
+++ data_scripts/clip.py
@@ -1,3 +1,6 @@
+import sys
+
+sys.path.append("./../DeepPlant/")
 from src.utils import create_path
 import os
 import numpy as np
@@ -7,6 +10,16 @@ from math import ceil
 from fastprogress import progress_bar
 from concurrent.futures import ProcessPoolExecutor
 import multiprocessing
+import argparse
+
+
+def parse_arguments(parser):
+    parser.add_argument("-i", "--input", type=str, help="path to the old npy files")
+    parser.add_argument(
+        "-o", "--output", type=str, help="path where to save the clipped files"
+    )
+    args = parser.parse_args()
+    return args
 
 
 def clip(value: float, threshold: Optional[float] = 50):
@@ -20,29 +33,31 @@ def save_file(filepath, data):
 
 
 def load_file(filepath):
-    return np.array(list(map(clip, np.load(filepath))))
+    return np.array(list(map(clip, np.load(filepath))), dtype=np.float32)
 
 
 def load_save_files(old_files):
     print("running now")
     for old_file in progress_bar(old_files):
-        old_filepath = os.path.join(labels_path, old_file)
-        new_filepath = os.path.join(new_labels_path, old_file)
-        if not os.path.exists(new_filepath):
-            cliped_labels = load_file(old_filepath)
-            save_file(new_filepath, cliped_labels)
+        if ".npy" in old_file:
+            old_filepath = os.path.join(labels_path, old_file)
+            new_filepath = os.path.join(new_labels_path, old_file)
+            if not os.path.exists(new_filepath) or True:
+                cliped_labels = load_file(old_filepath)
+                save_file(new_filepath, cliped_labels)
 
 
 # generate many data files in a directory
 def main():
-
+    parser = argparse.ArgumentParser(description="Clip numpy file using a threshold")
+    args = parse_arguments(parser)
     global labels_path
-    labels_path = "/s/chromatin/c/nobackup/deepplant/Data/Arabidopsis_thaliana/Non_Overlap_avg_2500_200_200/Labels_10kb_masked_2500"
+    labels_path = args.input
     # labels_path = "/s/chromatin/c/nobackup/deepplant/Data/Arabidopsis_thaliana/Non_Overlap_avg_4096_256_64/Labels_10kb_masked_4096"
     # labels_path = "/s/chromatin/c/nobackup/deepplant/Data/Arabidopsis_thaliana/Non_Overlap_avg/Labels_10kb_masked_2500"
 
     global new_labels_path
-    new_labels_path = "/s/chromatin/c/nobackup/deepplant/Data/Arabidopsis_thaliana/Non_Overlap_avg_2500_200_200/Labels_10kb_masked_2500_cliped"
+    new_labels_path = args.output
     # new_labels_path = "/s/chromatin/c/nobackup/deepplant/Data/Arabidopsis_thaliana/Non_Overlap_avg/Labels_10kb_masked_2500_cliped"
 
     # create a local directory to save files
